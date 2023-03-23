8ab7c4e42511db594724615f1a392deb89006caa
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-23 14:27:06 +0100

    add initial markup + array and forEach in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,48 @@ BONUS 3:
 Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 
 */
+
+//creare l'array contente gli oggetti composti da: un'immagine, un titolo ed una descrizione
+const videogames = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+
+//selezionare il container in dom dove inserire gli oggetti
+const carouselEl = document.querySelector('.my_carousel');
+//console.log(carouselEl);
+
+//usare il ciclo forEach per ciclare nell'array degli object
+videogames.forEach(game => {
+    //creare il markup da inserire dinamicamente
+    const markup = 
+    `
+    <div class="videogames">
+        <img class="active" src="./assets/${game.image}" alt="">
+        <div class="carousel_bottom rounded-bottom">
+            <h1 class="videogame_title ms-3 pt-2">${game.title}</h1>
+            <p class="videogame_description ms-3 mb-0 pb-2">${game.text}</p>
+        </div>
+    </div>
+    `
+});
